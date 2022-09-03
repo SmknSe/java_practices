1d2bac1ec55d71322362a255aa5dc2210338f89a
SmknSe <semeykin2003@mail.ru>
2022-09-03 21:23:15 +0300

    1

---
+++ src/practice3/Rectangle.java
@@ -0,0 +1,56 @@
+package practice3;
+
+public class Rectangle extends Shape{
+    protected double width;
+    protected double length;
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public Rectangle(String color, boolean filled, double width, double length) {
+        super(color, filled);
+        this.width = width;
+        this.length = length;
+    }
+
+    public Rectangle(double width, double length) {
+        this.width = width;
+        this.length = length;
+    }
+
+    public Rectangle() {
+    }
+
+    @Override
+    public double getArea() {
+        return width*length;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2*(length+width);
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "width=" + width +
+                ", length=" + length +
+                ", color='" + color + '\'' +
+                ", filled=" + filled +
+                '}';
+    }
+}
---
+++ src/practice3/Square.java
@@ -0,0 +1,39 @@
+package practice3;
+
+public class Square extends Rectangle{
+    public Square(String color, boolean filled, double side) {
+        super(color, filled, side, side);
+    }
+
+    public Square(double side) {
+        super(side, side);
+    }
+
+    public Square() {
+    }
+
+    public double getSide(){ return this.length;}
+    public void setSide(double side){
+        this.length = side;
+        this.width = side;
+    }
+
+    @Override
+    public void setLength(double side) {
+        setSide(side);
+    }
+
+    @Override
+    public void setWidth(double side) {
+        setSide(side);
+    }
+
+    @Override
+    public String toString() {
+        return "Square{" +
+                "side=" + width +
+                ", color='" + color + '\'' +
+                ", filled=" + filled +
+                '}';
+    }
+}
