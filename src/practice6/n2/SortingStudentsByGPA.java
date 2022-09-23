0ffffecce791bc5a2544c7fac912bc873fc9c9a7
SmknSe <semeykin2003@mail.ru>
2022-09-23 22:56:45 +0300

    1

--- src/practice6/n2/SortingStudentsByGPA.java
+++ src/practice6/n2/SortingStudentsByGPA.java
@@ -2,29 +2,10 @@ package practice6.n2;
 
 import java.util.Comparator;
 
-public class SortingStudentsByGPA implements Comparator<SortingStudentsByGPA> {
-    public String name;
-    public Integer age;
-    public Integer score;
-
-    SortingStudentsByGPA(String name,int age, int score){
-        this.name = name;
-        this.age = age;
-        this.score = score;
-    }
-
+public class SortingStudentsByGPA implements Comparator<Student> {
 
     @Override
-    public String toString() {
-        return "SortingStudentsByGPA{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                ", score=" + score +
-                '}';
-    }
-
-    @Override
-    public int compare(SortingStudentsByGPA o1, SortingStudentsByGPA o2) {
-        return o1.score.compareTo(o2.score);
+    public int compare(Student o1, Student o2) {
+        return o1.getScore().compareTo(o2.getScore());
     }
 }
--- src/practice6/n2/Test.java
+++ src/practice6/n2/Test.java
@@ -5,35 +5,35 @@ import java.util.Comparator;
 
 public class Test {
     public static void main(String[] args) {
-        SortingStudentsByGPA[] students = new SortingStudentsByGPA[5];
-        students[0] = new SortingStudentsByGPA("Adam",10,100);
-        students[1] = new SortingStudentsByGPA("Sam",12,12);
-        students[2] = new SortingStudentsByGPA("John",15,85);
-        students[3] = new SortingStudentsByGPA("Bob",13,60);
-        students[4] = new SortingStudentsByGPA("Bob",11,90);
+        Student[] students = new Student[5];
+        students[0] = new Student("Adam",10,100);
+        students[1] = new Student("Sam",12,12);
+        students[2] = new Student("John",15,85);
+        students[3] = new Student("Bob",13,60);
+        students[4] = new Student("Bob",11,90);
         quick_sort(students,0,4);
         for (int i=0;i<5;i++){
             System.out.println(students[i]);
         }
     }
 
-    public static void quick_sort(SortingStudentsByGPA[] list,int low, int high){
+    public static void quick_sort(Student[] list,int low, int high){
         if (list.length == 0)
             return;
         if (low >= high)
             return;
         int middle = low + (high - low) / 2;
-        SortingStudentsByGPA opora = list[middle];
+        Student opora = list[middle];
         int i = low, j = high;
         while (i <= j) {
-            while (opora.compare(list[i],opora)>0) {
+            while (opora.getSortingStudentsByGPA().compare(list[i],opora)>0) {
                 i++;
             }
-            while (opora.compare(list[j],opora)<0) {
+            while (opora.getSortingStudentsByGPA().compare(list[j],opora)<0) {
                 j--;
             }
             if (i <= j) {
-                SortingStudentsByGPA temp = list[i];
+                Student temp = list[i];
                 list[i] = list[j];
                 list[j] = temp;
                 i++;
